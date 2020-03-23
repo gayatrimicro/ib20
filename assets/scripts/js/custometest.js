d3eb279b90207c1a3c5dcbd1f51443b97ec8fac9
prashantgmicro <pgole31@gmail.com>
2020-03-23 13:54:28 +0530

    Content change

--- assets/scripts/js/custometest.js
+++ assets/scripts/js/custometest.js
@@ -6,13 +6,13 @@ myVar = parseInt(footer, 10);
     var l = Math.pow(10, Math.floor(Math.log(myVar)/Math.log(10)));
     var b = Math.floor(myVar/l);
     var digit = b-Math.floor(b/10)*10;
-    console.log(digit);
+    //console.log(digit);
 
         if (digit <= 5) {
             $('.shd_img').css('height','0%');
         }
 
-   if (digit <= 4){
+   if (digit <= 3){
     $('.shd_img2').css('height','0%');
    }
     }, true);
