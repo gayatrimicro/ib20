35bd8738cdd6eef066b467da6b4b0ca259c4ea36
Rits-theDeveloper <ritesh.mestry.naresh@gmail.com>
2020-06-02 10:35:07 +0530

    video

--- en/projects/bayswater.php
+++ en/projects/bayswater.php
@@ -504,7 +504,7 @@
 
 				<div class="o-section -padding-v-huge" style="background-color:#F5F5F5;">
 	<figure class="o-container"  data-theme="light">
-		<div class="c-fancy-video c-project_intro_item js-animate -paused has-video is-show" style="padding-bottom: 0% !important;">
+		<div class="c-fancy-video c-project_intro_item js-animate -paused has-video is-show" style="padding-bottom: 0% !important;margin-top: 0% !important;">
 			<div id="vid_seco">
 				<video id="desk_vid1" autoplay loop>
 				  <source src="../../assets/videos/bayswater1.mp4"  type="video/mp4">
@@ -553,16 +553,29 @@
 	</div>
 </div>
 
-				<div class="o-section -padding-v-huge" style="background-color:#FFFFFF;">
-	<figure class="o-container">
-		<div class="o-vimeo-player js-animate u-anim -delay-1" style="padding-bottom: 0% !important;">
-			<!-- <iframe class="js-animate" data-repeat data-callback="launch.VimeoPlayer()" data-module="VimeoPlayer" src="https://player.vimeo.com/video/330879106?controls=false&amp;autoplay=0&amp;transparent=false&amp;autopause=false&amp;loop=1&amp;muted=1" width="640" height="360" muted frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay; encrypted-media"></iframe> -->
-			<video id="normal_vid" autoplay loop>
+				<div class="o-section -padding-v-huge" style="background-color:#F5F5F5;">
+	<figure class="o-container"  data-theme="light">
+		<div class="c-fancy-video c-project_intro_item js-animate -paused has-video is-show" style="padding-bottom: 0% !important;margin-top: 0% !important;">
+			<div id="vid_tri">
+				<video id="normal_vid" autoplay loop>
 				  <source src="../../assets/videos/bayswater2.mp4"  type="video/mp4">
 				</video>
-			<!-- <video id="desk_vid" loop>
-			  <source src="../../assets/videos/bayswater.mov" type="video/mp4">
-			</video> -->
+				<div class="c-fancy-video-cursor || o-cursor_wrapper || js-video-cursor js-animate" data-module="CursorZone" data-repeat data-callback="trigger.CursorZone()">
+						<div class="c-fancy-video-cursor_zone || js-drag-zone"></div>
+
+						<div class="o-cursor -white -center-mobile || js-cursor">
+							<div class="o-cursor_background"></div>
+							<div class="o-cursor_label">
+								<div class="o-cursor_label_outer">
+									<div class="o-cursor_label_inner">
+										<span class="cur_txt">Play</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+			</div>
+			
 		</div>
 	</figure>
 </div>
@@ -1069,6 +1082,20 @@ $('#myid').click(function(){
 
 $('#vid_seco').click(function(){
 	var vid = document.getElementById("desk_vid1");	
+	if(vid.paused)
+	{
+		vid.play();
+		$(".cur_txt").text("Pause");
+	}
+	else
+	{
+		vid.pause();
+		$(".cur_txt").text("Play");
+	}
+});
+
+$('#vid_tri').click(function(){
+	var vid = document.getElementById("normal_vid");	
 	if(vid.paused)
 	{
 		vid.play();
